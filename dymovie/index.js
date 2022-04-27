9b5ccb3766fcd5814020bc1ef7b2f972300e72d9
Wendell Bitencourt <51727640+WBitencourt@users.noreply.github.com>
2022-04-27 17:22:08 -0300

    Delete dymovie to up a new version

--- dymovie/index.js
+++
@@ -1,7 +0,0 @@
-import {AppRegistry} from 'react-native';
-import Main from './src/screens/Main';
-import Login from './src/screens/Login';
-import Signup from './src/screens/Signup';
-import {name as appName} from './app.json';
-
-AppRegistry.registerComponent(appName, () => Signup);
